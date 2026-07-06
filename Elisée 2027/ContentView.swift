8b8e31b539613fdf8d3e14cfc5d7f3814a256ae3
clem <clement.perrousset@gmail.com>
2026-07-06 12:00:25 +0200

    Initial Commit

---
+++ Elisée 2027/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elisée 2027
+//
+//  Created by clément perrousset on 06/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elisée 2027/Elise_e_2027App.swift
@@ -0,0 +1,17 @@
+//
+//  Elise_e_2027App.swift
+//  Elisée 2027
+//
+//  Created by clément perrousset on 06/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Elise_e_2027App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
